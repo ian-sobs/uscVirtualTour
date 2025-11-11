cce8d85bf6017d8bc1e7824051e21f9588e974f3
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-11 23:26:37 +0800

    finished schema definition of buildings. only lacks relations definition

--- src/db/schema/buildings.psql.ts
+++ src/db/schema/buildings.psql.ts
@@ -1,10 +1,17 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { smallint, integer, serial, pgTable, varchar, index } from "drizzle-orm/pg-core";
 import { timestamps } from './columns.helpers';
+import { campuses } from "./campuses.psql";
+import { locations } from "./locations.psql";
 
 export const buildings = pgTable("buildings", {
-  id: integer().primaryKey(),
-  name: varchar({ length: 255 }).notNull(),
-  campus_id: integer(),
-  floor_array: integer().array(), // array of integers
-  ...timestamps
-});
+    id: serial().primaryKey(),
+    name: varchar({ length: 255 }).notNull(),
+    campus_id: integer().references(() => campuses.id),
+    location_id: integer().references(() => locations.id),
+    basement_count: smallint(),
+    floor_count: smallint(),
+    ...timestamps
+}, (table) => [
+    index("name_idx").on(table.name),
+    index("campus_id_idx").on(table.campus_id)
+]);
