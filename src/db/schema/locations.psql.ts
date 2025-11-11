b2136000b82ac5ef24c45ff149bf02532d043f57
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-11 22:17:32 +0800

    finished schema definition of locations table. it now only lacks relation definitions.

--- src/db/schema/locations.psql.ts
+++ src/db/schema/locations.psql.ts
@@ -1,10 +1,16 @@
-import { integer, pgTable, text, varchar } from "drizzle-orm/pg-core";
-import { timestamps } from './columns.helpers';
+import { integer, serial, pgTable, text, varchar, pgEnum, index } from "drizzle-orm/pg-core";
+
+import { campuses } from "./campuses.psql";
+
+export const categoryEnum = pgEnum('category', ['buildings', 'events', 'food', 'facilities', 'transport_parking', 'study_areas', 'dorms_residences', 'sports_recreation']);
 
 export const locations = pgTable("locations", {
-    id: integer().primaryKey(),
+    id: serial().primaryKey(),
     name: varchar({ length: 255 }).notNull(),
-    category: varchar({ length: 100 }),
+    category: categoryEnum(),
     description: text(),
-    campus_id: integer()
-});
+    campus_id: integer().references(() => campuses.id)
+}, (table) => [
+    index("name_idx").on(table.name),
+    index("category_campus_id_idx").on(table.category, table.campus_id)
+]);
