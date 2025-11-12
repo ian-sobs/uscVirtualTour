2b9f2bf38d99c7028ae5a6bf3e5e7100ee57374b
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-12 21:13:54 +0800

    now using timestamp with timezone and the date & time of the updateof a record is recorded

--- src/db/schema/columns.helpers.ts
+++ src/db/schema/columns.helpers.ts
@@ -3,6 +3,6 @@
 import { timestamp } from 'drizzle-orm/pg-core';
 
 export const timestamps = {
-  created_at: timestamp().defaultNow().notNull(),
-  updated_at: timestamp()
+    created_at: timestamp('created_at', { withTimezone: true }).defaultNow().notNull(),
+    updated_at: timestamp('updated_at', { withTimezone: true }).$onUpdate(() => new Date()),
 };
