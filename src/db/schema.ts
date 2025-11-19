1bc4aaf36bd2b23a10ea3dc756ea2c7e734413c4
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-19 23:57:58 +0800

    added users schema definition

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,21 @@
-import { smallint, integer, serial, pgTable, varchar, pgEnum, text, index } from "drizzle-orm/pg-core";
+import { smallint, integer, serial, pgTable, varchar, pgEnum, unique, text, index } from "drizzle-orm/pg-core";
 import {timestamps} from './columns.helpers'
+
+export const roleEnum = pgEnum('role', ['student', 'admin']);
+
+export const users = pgTable("users", {
+    id: serial().primaryKey(),
+    student_or_admin_id: integer(),
+    first_name: varchar({ length: 255 }).notNull(),
+    mid_name: varchar({ length: 255 }), 
+    last_name: varchar({ length: 255 }).notNull(),
+    password_hash: text(),
+    role: roleEnum(),
+    ...timestamps
+},(t) => [
+    index("last_name_idx").on(t.last_name),
+    unique("users_student_or_admin_id_role_composite_unique").on(t.student_or_admin_id, t.role)
+]);
 
 export const campuses = pgTable("campuses", {
   id: serial().primaryKey(),
@@ -37,4 +53,5 @@ export const buildings = pgTable("buildings", {
     index("building_name_idx").on(table.name),
     index("campus_idx").on(table.campus_id)
 ]);
+
 
