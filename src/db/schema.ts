e79f839aa191a6f7052c4d7f32e507b5d50bf006
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-19 23:56:09 +0800

    added locations schema definition

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { smallint, integer, serial, pgTable, varchar, text, index } from "drizzle-orm/pg-core";
+import { smallint, integer, serial, pgTable, varchar, pgEnum, text, index } from "drizzle-orm/pg-core";
 import {timestamps} from './columns.helpers'
 
 export const campuses = pgTable("campuses", {
@@ -10,6 +10,19 @@ export const campuses = pgTable("campuses", {
     index("campus_name_idx").on(table.name)
 ]);
 
+
+export const categoryEnum = pgEnum('category', ['buildings', 'events', 'food', 'facilities', 'transport_parking', 'study_areas', 'dorms_residences', 'sports_recreation']);
+
+export const locations = pgTable("locations", {
+    id: serial().primaryKey(),
+    name: varchar({ length: 255 }).notNull(),
+    category: categoryEnum(),
+    description: text(),
+    campus_id: integer().references(() => campuses.id, {onDelete: 'cascade'})
+}, (table) => [
+    index("location_name_idx").on(table.name),
+    index("category_campus_idx").on(table.category, table.campus_id)
+]);
 
 
 export const buildings = pgTable("buildings", {
