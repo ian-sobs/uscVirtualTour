71c44e05d3202bdb7b25289b960b2326362b169b
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-20 00:22:05 +0800

    added event_group_location_relations schema defn

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -163,3 +163,11 @@ export const event_location_relations = pgTable("event_location_relations", {
     primaryKey({ name: 'location_event_composite_pk', columns: [table.location_id, table.event_id] }),
     index("location_event_composite_idx").on(table.location_id, table.event_id)
 ]);
+
+export const event_group_location_relations = pgTable("event_group_location_relations", {
+  location_id: integer().references(() => locations.id, {onDelete: 'cascade'}),
+  event_group_id: integer().references(() => event_groups.id, {onDelete: 'cascade'})
+}, (table) => [
+    primaryKey({ name: 'location_event_group_composite_pk', columns: [table.location_id, table.event_group_id] }),
+    index('location_event_group_composite_idx').on(table.location_id, table.event_group_id)
+]);
