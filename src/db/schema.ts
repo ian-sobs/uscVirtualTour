bec05277be0feb3779a3381b144217db26817ac2
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-20 00:11:05 +0800

    added rooms schema defn

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -107,3 +107,15 @@ export const events = pgTable("events", {
     index("event_group_idx").on(table.event_group_id),
     index("org_idx").on(table.org_id)
 ]);
+
+export const rooms = pgTable("rooms", {
+    id: serial().primaryKey(),
+    name: varchar({length: 255}).notNull(),
+    building_id: integer().references(() => buildings.id, {onDelete: 'cascade'}),
+    office_id: integer().references(() => offices.id, {onDelete: 'set null'}),
+    description: text(),
+    floor_level: smallint()
+}, (table) => [
+    index("office_idx").on(table.office_id),
+    index("name_building_composite_idx").on(table.name, table.building_id)
+]);
