c0dcbc0dcf6bf31a6d115cc7fbc79de2e3da445a
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-20 00:19:02 +0800

    added event_room_relations schema defn

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -147,3 +147,11 @@ export const rooms = pgTable("rooms", {
     index("office_idx").on(table.office_id),
     index("name_building_composite_idx").on(table.name, table.building_id)
 ]);
+
+export const event_room_relations = pgTable("event_room_relations", {
+    event_id: integer().references(() => events.id, {onDelete: 'cascade'}),
+    room_id: integer().references(() => rooms.id, {onDelete: 'cascade'})
+}, (table) => [
+    primaryKey({ name: 'event_room_composite_pk', columns: [table.event_id, table.room_id] }),
+    index("event_room_composite_idx").on(table.event_id, table.room_id)
+]);
