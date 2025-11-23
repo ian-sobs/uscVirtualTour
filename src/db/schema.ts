3e61846c9aa17bf963c1550c169fa124e07a50f5
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-24 04:54:07 +0800

    user permissions within the org now have a default value of false

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -48,9 +48,9 @@ export const organizations = pgTable("organizations", {
 export const user_org_relations = pgTable("user_org_relations", {
     user_id: text().references(() => users.id, {onDelete: 'cascade'}),
     org_id: integer().references(() => organizations.id, {onDelete: 'cascade'}),
-    can_post_events: boolean(),
-    can_add_members: boolean(),
-    can_remove_members: boolean()
+    can_post_events: boolean().default(false).notNull(),
+    can_add_members: boolean().default(false).notNull(),
+    can_remove_members: boolean().default(false).notNull()
 }, (table) => [
     primaryKey({ name: 'user_org_composite_pk', columns: [table.user_id, table.org_id] }),
     index("user_org_composite_idx").on(table.user_id, table.org_id)
