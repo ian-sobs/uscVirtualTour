9525e926ae5d95292e172ac59b8c98634486ec31
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-28 22:40:28 +0800

    added helper functions for getting a user's role and any orgs they are members of

--- src/app/api/utils/auth.ts
+++ src/app/api/utils/auth.ts
@@ -1,6 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
+import { authUser } from "@/types";
+import { UserRole } from '@/types';
+import { db } from '@/index';
+import { user_org_relations } from '@/db/schema';
+import { eq } from 'drizzle-orm';
+
 
 export async function checkAuth(request: NextRequest) {
   const session = await auth.api.getSession({
@@ -8,6 +14,19 @@ export async function checkAuth(request: NextRequest) {
   });
 
   return session;
+}
+
+export async function minAuthStudent(request: NextRequest) {
+  const session = await checkAuth(request);
+  
+  if (!session?.user) {
+    return NextResponse.json(
+      { error: 'Unauthorized. Please sign in.' },
+      { status: 401 }
+    );
+  }
+
+  return null; // No error, user is at minimum have student auth
 }
 
 export async function requireAdmin(request: NextRequest) {
@@ -29,4 +48,27 @@ export async function requireAdmin(request: NextRequest) {
   }
 
   return null; // No error, user is admin
+}
+
+
+export const getUserRole = (userObj: authUser): UserRole => {
+  if(!userObj.is_admin && !userObj.is_student){
+    return 'guest'
+  }
+  else if(userObj.is_student){
+    return 'student'
+  }
+  else {
+    return 'admin'
+  }
+};
+
+export async function getUserOrgs(userObj: authUser) {
+  const userOrgsTemp = await db.select({
+    org_id: user_org_relations.org_id
+  }).from(user_org_relations).where(eq(user_org_relations.user_id, userObj.id))
+
+  const userOrgs: number[] = []
+  userOrgsTemp.map((org) => userOrgs.push(org.org_id))
+  return userOrgs;
 }
