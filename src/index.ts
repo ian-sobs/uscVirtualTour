4838aee580154d898c542dd35fbd254507ae07e2
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-29 02:22:46 +0800

    added this line so i can use relational queries

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { Pool } from "pg";
+import { schema } from './db/schema';
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-export const db = drizzle({ client: pool });
+export const db = drizzle({ client: pool , schema: schema});
