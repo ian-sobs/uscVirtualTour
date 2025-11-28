68de5b4aa506aa8e8cf8953ce6241ef5221f9620
Ian Sobrecaray <ian@sobrecaray.com>
2025-11-28 23:51:05 +0800

    wrapped the logic in a try catch block and retured the data

--- src/app/api/orgs/[orgId]/events/route.ts
+++ src/app/api/orgs/[orgId]/events/route.ts
@@ -7,46 +7,61 @@ import { checkAuth } from '@/app/api/utils/auth';
 import { getUserOrgs } from '@/app/api/utils/auth';
 
 // GET /orgs/:orgId/events - Get all events in a given org
-export async function GET(request: NextRequest, ctx: RouteContext<'/api/orgs/[orgId]/events'>) {
-    const session = await checkAuth(request)
-    if(!session){
+export async function GET(
+    request: NextRequest, 
+    { params }: { params: Promise<{ orgId: string }> }) {
+
+    try {
+        const session = await checkAuth(request)
+        if(!session){
+            return NextResponse.json(
+                { error: "Unauthorized" },
+                { status: 401 }
+            );
+        }
+        const userRole = getUserRole(session.user)
+        
+        const { orgId } = await params
+        const searchParams = request.nextUrl.searchParams
+        const dateTimeStart = searchParams.get('dateTimeStart')
+        const dateTimeEnd = searchParams.get('dateTimeEnd')
+        const name = searchParams.get('name')
+
+        const filters: SQL[] = [];
+
+        if(dateTimeStart) filters.push(eq(events.date_time_start, new Date(dateTimeStart)));
+        if(dateTimeEnd) filters.push(eq(events.date_time_end, new Date(dateTimeEnd)));
+
+        if(name) filters.push(ilike(events.name, `${name}%`));
+
+        filters.push(eq(events.org_id, parseInt(orgId)));
+        if(userRole === 'guest'){ 
+            filters.push(eq(events.visibility, "everyone"))
+        }
+        else if(userRole === 'student'){
+            filters.push(inArray(events.visibility, ["everyone", "only_students"]))
+        }
+
+        const result = await db.select({
+            id: events.id,
+            name: events.name,
+            date_time_start: events.date_time_start,
+            date_time_end: events.date_time_end,
+            custom_marker: events.custom_marker,
+            org_id: events.org_id,
+            visibility: events.visibility
+        }).from(events).where(and(...filters));
+
+        return NextResponse.json({ data: result });
+        
+    } catch (err) {
+        console.error(err);
         return NextResponse.json(
-            { error: "Unauthorized" },
-            { status: 401 }
+            { error: "Internal Server Error" },
+            { status: 500 }
         );
     }
-    const userRole = getUserRole(session.user)
-    
-    const { orgId } = await ctx.params
-    const searchParams = request.nextUrl.searchParams
-    const dateTimeStart = searchParams.get('dateTimeStart')
-    const dateTimeEnd = searchParams.get('dateTimeEnd')
-    const name = searchParams.get('name')
 
-    const filters: SQL[] = [];
-
-    if(dateTimeStart) filters.push(eq(events.date_time_start, new Date(dateTimeStart)));
-    if(dateTimeEnd) filters.push(eq(events.date_time_end, new Date(dateTimeEnd)));
-
-    if(name) filters.push(ilike(events.name, `${name}%`));
-
-    filters.push(eq(events.org_id, parseInt(orgId)));
-    if(userRole === 'guest'){ 
-        filters.push(eq(events.visibility, "everyone"))
-    }
-    else if(userRole === 'student'){
-        filters.push(inArray(events.visibility, ["everyone", "only_students"]))
-    }
-
-    const result = await db.select({
-        id: events.id,
-        name: events.name,
-        date_time_start: events.date_time_start,
-        date_time_end: events.date_time_end,
-        custom_marker: events.custom_marker,
-        org_id: events.org_id,
-        visibility: events.visibility
-    }).from(events).where(and(...filters));
 }
 
 
