cb9c2a285b41de82b69b6be93eeecef602ea6a6f
Ian Sobrecaray <ian@sobrecaray.com>
2025-12-11 23:43:29 +0800

    Fixed the issue of having different slug names

--- src/app/api/eventGrps/[eventGrpId]/locations/[locationId]/route.ts
+++ src/app/api/eventGrps/[eventGrpId]/locations/[locationId]/route.ts
@@ -56,3 +56,91 @@ export async function DELETE(request: NextRequest,
         );
     }
 }
+
+export async function PATCH(
+    request: NextRequest, 
+    { params }: { params: Promise<{ eventGrpId: string; locationId: string }> }){
+
+    try {
+        const pathParams = await params
+
+        const eventGroupId = (!isNumericString(pathParams.eventGrpId)) ? null : parseInt(pathParams.eventGrpId)
+        const oldLocationId = (!isNumericString(pathParams.locationId)) ? null : parseInt(pathParams.locationId)
+
+        if(eventGroupId == null){
+            return NextResponse.json(
+                { error: "Provide a valid numerical value for eventGroupId" },
+                { status: 400 }
+            );     
+        }
+        if(oldLocationId == null){
+            return NextResponse.json(
+                { error: "Provide a valid numerical value for oldLocationId" },
+                { status: 400 }
+            );     
+        }
+
+        const session = await checkAuth(request)
+        if(!session){
+            return NextResponse.json(
+                { error: "Unauthorized" },
+                { status: 401 }
+            );        
+        }
+
+        const userRole = getUserRole(session.user)
+
+        if(userRole != "admin"){
+            return NextResponse.json(
+                { error: "Unauthorized" },
+                { status: 401 }
+            );          
+        }
+
+
+        const body: {
+            locationId: any
+        } = await request.json();
+
+        const locationId = isNumericString(body.locationId) ? parseInt(body.locationId) : null;
+
+        if (locationId === null){
+            return NextResponse.json(
+                {
+                    error: "'locationId' cannot be null."
+                },
+                { status: 400 }
+            );
+        }
+
+        const result = await db.update(event_group_location_relations).set({
+            location_id: locationId
+        }).where(
+            and(
+                eq(event_group_location_relations.event_group_id, eventGroupId),
+                eq(event_group_location_relations.location_id, oldLocationId)
+            )
+        ).returning({
+            eventGroupId: event_group_location_relations.event_group_id,
+            locationId: event_group_location_relations.location_id
+        })
+
+        if(result.length <= 0){
+            return NextResponse.json(
+                { error: "No event group with given eventGrpId had a location with given oldLocationId exists." },
+                { status: 404 }
+            );    
+        }
+    
+        return NextResponse.json({
+            data: result[0]
+        })
+
+    } catch (err){
+        console.error(err);
+        return NextResponse.json(
+            { error: "Internal Server Error" },
+            { status: 500 }
+        ); 
+    }
+}
