21dc0487b3e366233e44ab1dd1db983ee70d7751
clandy07 <edelcortes3@gmail.com>
2025-12-13 20:51:27 +0800

    fix: Fixed drizzle issue wherein duplicate changes to the schema are reflected

--- drizzle/20251211153533_worried_skaar.sql
+++ drizzle/20251211153533_worried_skaar.sql
@@ -1,13 +1,2 @@
-CREATE TABLE "geometries" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"polygon" geometry(point)
-);
---> statement-breakpoint
 ALTER TABLE "user_org_relations" ALTER COLUMN "user_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "user_org_relations" ALTER COLUMN "org_id" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "organizations" ADD COLUMN "name" varchar(150) NOT NULL;--> statement-breakpoint
-ALTER TABLE "organizations" ADD COLUMN "description" text;--> statement-breakpoint
-ALTER TABLE "rooms" ADD COLUMN "geometry_id" integer;--> statement-breakpoint
-ALTER TABLE "user_org_relations" ADD COLUMN "can_set_member_permissions" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-CREATE INDEX "polygon_idx" ON "geometries" USING gist ("polygon");--> statement-breakpoint
-ALTER TABLE "rooms" ADD CONSTRAINT "rooms_geometry_id_geometries_id_fk" FOREIGN KEY ("geometry_id") REFERENCES "public"."geometries"("id") ON DELETE set null ON UPDATE no action;
+ALTER TABLE "user_org_relations" ALTER COLUMN "org_id" SET NOT NULL;
