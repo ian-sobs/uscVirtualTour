37e36de7e98ed734cfeac77d5d4a52c7acfb7d7a
Eduardo Cortes <edelcortes3@gmail.com>
2025-12-15 22:59:35 +0800

    feat: 'Help & Support' button now works and redirects users to a Google Form

--- src/app/components/Profile/ProfileMenu.tsx
+++ src/app/components/Profile/ProfileMenu.tsx
@@ -170,7 +170,10 @@ export default function ProfileMenu({ mapType, onMapTypeChange }: ProfileMenuPro
 								<option value="terrain">Terrain</option>
 							</select>
 						</div>
-					<button className="w-full px-4 py-2 text-left hover:bg-green-50 text-black flex items-center gap-3 cursor-pointer">
+					<button 
+						onClick={() => window.open('https://docs.google.com/forms/d/e/1FAIpQLSfjWuNZGvBVQnA4U5fhCbqhZMjrEnZ5C9oMOua0IjyaQ4fGxw/viewform?usp=dialog', '_blank')}
+						className="w-full px-4 py-2 text-left hover:bg-green-50 text-black flex items-center gap-3 cursor-pointer"
+					>
 						<svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 						</svg>
